51ee7351ba4a60f5c94335fcfd2a09a7100cd709
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-05 19:53:20 +0200

    included short name in manifest

--- src/app.config.js
+++ src/app.config.js
@@ -2,6 +2,7 @@
 
 const Config = {
 	appTitle: 'Full Stack Weekly',
+	appTitleShort: 'FullStackWeekly',
 	appDescription: 'A free weekly newsletter for the modern full-stack developer.',
 	appThemeColor: '#287ab1',
 	appBgColor: '#ffffff',
