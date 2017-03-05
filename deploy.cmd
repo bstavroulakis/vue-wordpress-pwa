465b2e65700b420973a773b540a35d6fea011f11
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-05 14:23:52 +0200

    included azure deployment files

---
+++ deploy.cmd
@@ -0,0 +1,30 @@
+call npm install 
+IF "%ERRORLEVEL%" NEQ "0" goto error
+
+call npm run build
+IF "%ERRORLEVEL%" NEQ "0" goto error
+
+del /q %DEPLOYMENT_TARGET%\*
+for /d %%x in (%DEPLOYMENT_TARGET%\*) do @rd /s /q "%%x"
+IF "%ERRORLEVEL%" NEQ "0" goto error
+
+xcopy %DEPLOYMENT_SOURCE%\dist %DEPLOYMENT_TARGET% /Y /E
+IF "%ERRORLEVEL%" NEQ "0" goto error
+
+goto end
+
+:error
+endlocal
+echo An error has occurred during web site deployment.  
+call :exitSetErrorLevel  
+call :exitFromFunction 2>nul
+
+:exitSetErrorLevel
+exit /b 1
+
+:exitFromFunction
+()
+
+:end
+endlocal  
+echo Finished successfully.  
