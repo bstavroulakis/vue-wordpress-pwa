11c3db740dd88f7a23b00ae7f89aa3ec9e742ecd
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-06 13:36:42 +0200

    personal card spacing

--- src/theme/Category.vue
+++ src/theme/Category.vue
@@ -2,7 +2,7 @@
   <section id="ThemeCategory">
     <div class="columns" v-if="routeParamId == 'blog'">
       <div class="column">
-        <div class="columns personal-card">
+        <div class="columns personal-card card">
           <p class="column personal-img"><img src="https://api.fullstackweekly.com/wp-content/uploads/2016/11/200x200.jpg" alt="Bill Stavroulakis" width="100"></p>
           <p class="column is-three-quarters personal-desc">Hi, I’m <a href="https://twitter.com/bstavroulakis" target="_blank">Bill Stavroulakis</a>, many years ago my journey started on this thing called Web Development.<br/><br/>
           Over here you can find all of the interesting things I find on my way.
@@ -10,6 +10,7 @@
         </div>
       </div>
     </div>
+    <div class="columns"><div class="column"></div></div>
     <div class="columns">
       <div class="column" v-if="category.name">
         <h2>
