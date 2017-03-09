07189e7533dd9fbb1cb8d031bef31e9c061b9507
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-09 19:34:02 +0200

    card clearfix

--- src/theme/Category.vue
+++ src/theme/Category.vue
@@ -4,11 +4,12 @@
       <div class="column personal-img"><img src="https://api.fullstackweekly.com/wp-content/uploads/2016/11/200x200.jpg" alt="Bill Stavroulakis" width="100"></div>
       <div class="column is-three-quarters personal-desc">
         Hi, I’m <a href="https://twitter.com/bstavroulakis" rel="noopener" target="_blank">Bill Stavroulakis</a>, many years ago my journey started on this thing called Web Development.<br/><br/>
-        <span class="is-pulled-left">Over here you can find all of the interesting things I find on my way.</span><span class="is-pulled-left">&nbsp;</span>
+        <span class="is-pulled-left">Over here you can find all of the interesting things I find on my way.&nbsp;</span>
         <span class="is-pulled-left">This website is part of the <a href="https://github.com/bstavroulakis/vue-wordpress-pwa">vue-wordpress-pwa</a> project.</span>
         <div class="github-star">
           <iframe src="https://ghbtns.com/github-btn.html?user=bstavroulakis&repo=vue-wordpress-pwa&type=star&count=true" frameborder="0" scrolling="0" width="170px" height="20px"></iframe>
         </div>
+        <div class="is-clearfix"></div>
       </div>
     </div>
     <div class="columns"><div class="column">
