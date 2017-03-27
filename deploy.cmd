391b37cd3603f0a863eac1af5cc7576e5740987e
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-28 01:49:26 +0300

    Node.js Deploy

--- deploy.cmd
+++ deploy.cmd
@@ -4,6 +4,13 @@ IF "%ERRORLEVEL%" NEQ "0" goto error
 call npm run build
 IF "%ERRORLEVEL%" NEQ "0" goto error
 
+del /q %DEPLOYMENT_TARGET%\*
+for /d %%x in (%DEPLOYMENT_TARGET%\*) do @rd /s /q "%%x"
+IF "%ERRORLEVEL%" NEQ "0" goto error
+
+xcopy %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET% /Y /E
+IF "%ERRORLEVEL%" NEQ "0" goto error
+
 goto end
 
 :error
