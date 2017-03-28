6f06d35ede325583389eb7009397c8e483112803
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-28 18:04:30 +0300

    don't include node_modules in copy-paste

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -9,13 +9,16 @@ for /d %%x in (%DEPLOYMENT_TARGET%\*) do @rd /s /q "%%x"
 IF "%ERRORLEVEL%" NEQ "0" goto error
 
 :: xcopy %DEPLOYMENT_SOURCE%\* %DEPLOYMENT_TARGET% /Y /E
-%DEPLOYMENT_SOURCE%\build\7za.exe a -t7z %DEPLOYMENT_SOURCE%\deploy.7z %DEPLOYMENT_SOURCE%\*  
+%DEPLOYMENT_SOURCE%\build\7za.exe a -t7z %DEPLOYMENT_SOURCE%\deploy.7z %DEPLOYMENT_SOURCE%\* -mx0 -xr!node_modules
 IF "%ERRORLEVEL%" NEQ "0" goto error
 
 %DEPLOYMENT_SOURCE%\build\7za.exe -y x %DEPLOYMENT_SOURCE%\deploy.7z -o%DEPLOYMENT_TARGET%
 IF "%ERRORLEVEL%" NEQ "0" goto error
 
 del /q %DEPLOYMENT_SOURCE%\deploy.7z
+IF "%ERRORLEVEL%" NEQ "0" goto error
+
+call npm install --prefix %DEPLOYMENT_TARGET%/node_modules
 IF "%ERRORLEVEL%" NEQ "0" goto error
 
 goto end
