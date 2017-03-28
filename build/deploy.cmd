d599c0c695541a18068c8d5a8a72618a9afa9c95
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-28 18:21:24 +0300

    move only necessary files on deploy

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -10,15 +10,18 @@ for /d %%I in (%DEPLOYMENT_TARGET%\*) do (
 )
 IF "%ERRORLEVEL%" NEQ "0" goto error
 
-:: xcopy %DEPLOYMENT_SOURCE%\* %DEPLOYMENT_TARGET% /Y /E
-%DEPLOYMENT_SOURCE%\build\7za.exe a -t7z %DEPLOYMENT_SOURCE%\deploy.7z %DEPLOYMENT_SOURCE%\* -mx0 -xr!node_modules -xr!.git
-IF "%ERRORLEVEL%" NEQ "0" goto error
+xcopy %DEPLOYMENT_SOURCE%\dist %DEPLOYMENT_TARGET%\dist /Y /E
+xcopy %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html /Y /E
+xcopy %DEPLOYMENT_SOURCE%\web.config %DEPLOYMENT_TARGET%\web.config /Y /E
+xcopy %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js /Y /E
+:: %DEPLOYMENT_SOURCE%\build\7za.exe a -t7z %DEPLOYMENT_SOURCE%\deploy.7z %DEPLOYMENT_SOURCE%\* -mx0 -xr!node_modules -xr!.git
+:: IF "%ERRORLEVEL%" NEQ "0" goto error
 
-%DEPLOYMENT_SOURCE%\build\7za.exe -y x %DEPLOYMENT_SOURCE%\deploy.7z -o%DEPLOYMENT_TARGET%
-IF "%ERRORLEVEL%" NEQ "0" goto error
+:: %DEPLOYMENT_SOURCE%\build\7za.exe -y x %DEPLOYMENT_SOURCE%\deploy.7z -o%DEPLOYMENT_TARGET%
+:: IF "%ERRORLEVEL%" NEQ "0" goto error
 
-del /q %DEPLOYMENT_SOURCE%\deploy.7z
-IF "%ERRORLEVEL%" NEQ "0" goto error
+:: del /q %DEPLOYMENT_SOURCE%\deploy.7z
+:: IF "%ERRORLEVEL%" NEQ "0" goto error
 
 call npm install --prefix %DEPLOYMENT_TARGET%/node_modules
 IF "%ERRORLEVEL%" NEQ "0" goto error
