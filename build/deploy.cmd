dab4d9ab4ba9c3bfd0fd55569466ca70a1fcbb80
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-28 19:27:13 +0300

    node_modules fix

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -6,7 +6,7 @@ IF "%ERRORLEVEL%" NEQ "0" goto error
 
 del /q %DEPLOYMENT_TARGET%\*
 for /d %%I in (%DEPLOYMENT_TARGET%\*) do (
-    if /i not "%%~nxI" equ "node_modules" @rd /s /q "%%~I"
+    @rd /s /q "%%~I"
 )
 IF "%ERRORLEVEL%" NEQ "0" goto error
 
@@ -14,14 +14,14 @@ xcopy %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /i
 xcopy %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html*
 xcopy %DEPLOYMENT_SOURCE%\web.config %DEPLOYMENT_TARGET%\web.config*
 xcopy %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js*
-:: %DEPLOYMENT_SOURCE%\build\7za.exe a -t7z %DEPLOYMENT_SOURCE%\deploy.7z %DEPLOYMENT_SOURCE%\* -mx0 -xr!node_modules -xr!.git
-:: IF "%ERRORLEVEL%" NEQ "0" goto error
+%DEPLOYMENT_SOURCE%\build\7za.exe a -t7z %DEPLOYMENT_SOURCE%\deploy.7z %DEPLOYMENT_SOURCE%\node_modules
+IF "%ERRORLEVEL%" NEQ "0" goto error
 
-:: %DEPLOYMENT_SOURCE%\build\7za.exe -y x %DEPLOYMENT_SOURCE%\deploy.7z -o%DEPLOYMENT_TARGET%
-:: IF "%ERRORLEVEL%" NEQ "0" goto error
+%DEPLOYMENT_SOURCE%\build\7za.exe -y x %DEPLOYMENT_SOURCE%\deploy.7z -o%DEPLOYMENT_TARGET%
+IF "%ERRORLEVEL%" NEQ "0" goto error
 
-:: del /q %DEPLOYMENT_SOURCE%\deploy.7z
-:: IF "%ERRORLEVEL%" NEQ "0" goto error
+del /q %DEPLOYMENT_SOURCE%\deploy.7z
+IF "%ERRORLEVEL%" NEQ "0" goto error
 
 call npm install --prefix %DEPLOYMENT_TARGET%/node_modules
 IF "%ERRORLEVEL%" NEQ "0" goto error
