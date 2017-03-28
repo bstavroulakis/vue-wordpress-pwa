4b1344f435c5bbe00e9a58c149834f8a74ac6a14
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-28 16:00:57 +0300

    speed up deployment with 7za

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -9,7 +9,7 @@ for /d %%x in (%DEPLOYMENT_TARGET%\*) do @rd /s /q "%%x"
 IF "%ERRORLEVEL%" NEQ "0" goto error
 
 :: xcopy %DEPLOYMENT_SOURCE%\* %DEPLOYMENT_TARGET% /Y /E
-7za %DEPLOYMENT_SOURCE%\*  %DEPLOYMENT_TARGET%\deploy.zip
+%DEPLOYMENT_SOURCE%\build\7za.exe %DEPLOYMENT_SOURCE%\*  %DEPLOYMENT_TARGET%\deploy.zip
 IF "%ERRORLEVEL%" NEQ "0" goto error
 
 goto end
