f0e6f0adae9ce119a2e5c854ccc9f30c672e5c2b
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-28 20:26:08 +0300

    node modules only ones in production

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -14,19 +14,10 @@ xcopy %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /i
 xcopy %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html*
 xcopy %DEPLOYMENT_SOURCE%\web.config %DEPLOYMENT_TARGET%\web.config*
 xcopy %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js*
-xcopy %DEPLOYMENT_SOURCE%\node_modules\* %DEPLOYMENT_TARGET%\node_modules /s /i
+:: xcopy %DEPLOYMENT_SOURCE%\node_modules\* %DEPLOYMENT_TARGET%\node_modules /s /i
 
-:: %DEPLOYMENT_SOURCE%\build\7za.exe a -t7z %DEPLOYMENT_SOURCE%\deploy.7z %DEPLOYMENT_SOURCE%\node_modules
-:: IF "%ERRORLEVEL%" NEQ "0" goto error
-
-:: %DEPLOYMENT_SOURCE%\build\7za.exe -y x %DEPLOYMENT_SOURCE%\deploy.7z -o%DEPLOYMENT_TARGET%
-:: IF "%ERRORLEVEL%" NEQ "0" goto error
-
-:: del /q %DEPLOYMENT_SOURCE%\deploy.7z
-:: IF "%ERRORLEVEL%" NEQ "0" goto error
-
-:: call npm install --prefix %DEPLOYMENT_TARGET%/node_modules
-:: IF "%ERRORLEVEL%" NEQ "0" goto error
+call npm install --only=production --prefix %DEPLOYMENT_TARGET%
+IF "%ERRORLEVEL%" NEQ "0" goto error
 
 goto end
 
