24d48ae2a4e4c83ac974f5fcc1cc7ba142e43bd8
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-29 14:02:31 +0300

    node_modules in target folder

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -14,9 +14,11 @@ xcopy %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /i
 xcopy %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html*
 xcopy %DEPLOYMENT_SOURCE%\web.config %DEPLOYMENT_TARGET%\web.config*
 xcopy %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js*
+xcopy %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json*
 :: xcopy %DEPLOYMENT_SOURCE%\node_modules\* %DEPLOYMENT_TARGET%\node_modules /s /i
 
-call npm install --prefix %DEPLOYMENT_TARGET%\node_modules -g --only=production
+call cd %DEPLOYMENT_TARGET% 
+call npm install --only=production
 IF "%ERRORLEVEL%" NEQ "0" goto error
 
 goto end
