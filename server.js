0f964edb105cd4d7962f3eb6158f334e07ea8bb7
Bill Stavroulakis <bstavroulakis@gmail.com>
2017-03-29 18:00:33 +0300

    content length for gzip

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 process.env.VUE_ENV = 'server'
 const isProd = !(process.env.NODE_ENV === 'development')
 const cdnClient = (isProd) ? 'https://fullstackweekly-client.azureedge.net' : '/assets'
-
 const fs = require('fs')
 const path = require('path')
 const resolve = file => path.resolve(__dirname, file)
@@ -11,10 +10,7 @@ const express = require('express')
 const compression = require('compression')
 const favicon = require('serve-favicon')
 const serialize = require('serialize-javascript')
-
 const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
-
-const app = express()
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'fullstackweekly-client.azureedge.net');
@@ -33,23 +29,36 @@ var cacheControl = function(req, res, next) {
   next()
 }
 
+var strictTransportSecurity = function(req, res, next) {
+  res.header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload')
+  next()
+}
+
+// parse index.html template
+const indexHTML = (() => {
+  let template = fs.readFileSync(resolve('./index.html'), 'utf-8')
+  template = template.replace(/{{ CDN }}/gmi, cdnClient)
+  return template
+})()
+
+function createRenderer (bundle) {
+  return createBundleRenderer(bundle)
+}
+
+const app = express()
 app.disable('x-powered-by')
 app.use(compression())
+app.use(strictTransportSecurity)
 app.use(cacheControl)
 app.use(allowCrossDomain)
 
-// parse index.html template
-const html = (() => {
-  let template = fs.readFileSync(resolve('./index.html'), 'utf-8')
-  template = template.replace(/{{ CDN }}/gmi, cdnClient)
-  const i = template.indexOf('{{ APP }}')
-  // styles are injected dynamically via vue-style-loader in development
-  // const style = isProd ? '<link rel="stylesheet" href="/dist/styles.css">' : ''
-  return {
-    head: template.slice(0, i).replace('{{ STYLE }}', ''),
-    tail: template.slice(i + '{{ APP }}'.length)
-  }
-})()
+if (isProd) {
+  app.use('/', express.static(resolve('./dist')))
+  app.use(favicon(path.resolve(__dirname, './dist/favicon.ico')))
+} else {
+  app.use('/dist', express.static(resolve('./dist'))) 
+  app.use(favicon(path.resolve(__dirname, 'src/assets/favicon.ico')))
+}
 
 let renderer
 if (isProd) {
@@ -62,23 +71,6 @@ if (isProd) {
   })
 }
 
-function createRenderer (bundle) {
-  return createBundleRenderer(bundle, {
-    cache: require('lru-cache')({
-      max: 1000,
-      maxAge: 1000 * 60 * 15
-    })
-  })
-}
-
-if (isProd) {
-  app.use('/', express.static(resolve('./dist')))
-  app.use(favicon(path.resolve(__dirname, './dist/favicon.ico')))
-} else {
-  app.use('/dist', express.static(resolve('./dist'))) 
-  app.use(favicon(path.resolve(__dirname, 'src/assets/favicon.ico')))
-}
-
 app.get('*', (req, res) => {
   if (!renderer) {
     return res.end('waiting for compilation... refresh in a moment.')
@@ -86,34 +78,18 @@ app.get('*', (req, res) => {
 
   var s = Date.now()
   const context = { url: req.url }
-  const renderStream = renderer.renderToStream(context)
-  let firstChunk = true
-
-  res.write(html.head)
-
-  renderStream.on('data', chunk => {
-    if (firstChunk) {
-      // embed initial store state
-      if (context.initialState) {
-        res.write(
-          `<script>window.__INITIAL_STATE__=${
-            serialize(context.initialState, { isJSON: true })
-          }</script>`
-        )
-      }
-      firstChunk = false
+  renderer.renderToString(context, (err, html) => {
+    if (err) {
+      console.log('Error rendering to string: ');
+      console.log(err);
+      console.log(err.message);
+      return res.status(200).send('Server Error');
     }
-    res.write(chunk)
-  })
-
-  renderStream.on('end', () => {
-    res.end(html.tail)
-    console.log(`whole request: ${Date.now() - s}ms`)
-  })
-
-  renderStream.on('error', err => {
-    console.log(err)
-    throw err
+    html = indexHTML.replace('{{ APP }}', html);
+    html = html.replace('{{ STATE }}', `<script>window.__INITIAL_STATE__=${serialize(context.initialState, { isJSON: true })}</script>`);
+    res.setHeader('Content-Length', Buffer.byteLength(html));
+    res.write(html);
+    res.end();
   })
 })
 
